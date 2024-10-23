f7da603bf199bf739df993a4e5bdf86516baabe6
Rosie Wood <rwood@turing.ac.uk>
2024-10-23 12:02:46 +0100

    remove slash from src

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ setup(
     version="0.0.1",
     url="https://github.com/maps-as-data/MapTextPipeline",
     description="Text Detection and Recognition framework based on AdelaiDet and detectron2.",
-    packages=find_packages("src/"),
+    packages=find_packages("src"),
     package_dir = {"": "src"},
     python_requires=">=3.9",
     install_requires=[
